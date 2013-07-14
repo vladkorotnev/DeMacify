b0fc844dad43d1434638c58158e585f69779e82f
Akasaka Ryuunosuke <vladkorotnev@gmail.com>
2013-07-14 15:08:27 +0600

    initial

---
+++ DeMacify/Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.perform = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.unTrash = New System.Windows.Forms.CheckBox()
+        Me.unFsevents = New System.Windows.Forms.CheckBox()
+        Me.unUnder = New System.Windows.Forms.CheckBox()
+        Me.unDSS = New System.Windows.Forms.CheckBox()
+        Me.dir = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.folder = New System.Windows.Forms.FolderBrowserDialog()
+        Me.process = New System.Windows.Forms.ProgressBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'perform
+        '
+        Me.perform.Enabled = False
+        Me.perform.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.perform.Location = New System.Drawing.Point(12, 151)
+        Me.perform.Name = "perform"
+        Me.perform.Size = New System.Drawing.Size(268, 23)
+        Me.perform.TabIndex = 0
+        Me.perform.Text = "<select a folder first>"
+        Me.perform.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.unTrash)
+        Me.GroupBox1.Controls.Add(Me.unFsevents)
+        Me.GroupBox1.Controls.Add(Me.unUnder)
+        Me.GroupBox1.Controls.Add(Me.unDSS)
+        Me.GroupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 42)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(268, 103)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        '
+        'unTrash
+        '
+        Me.unTrash.AutoSize = True
+        Me.unTrash.Checked = True
+        Me.unTrash.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.unTrash.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.unTrash.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.unTrash.Location = New System.Drawing.Point(6, 81)
+        Me.unTrash.Name = "unTrash"
+        Me.unTrash.Size = New System.Drawing.Size(254, 16)
+        Me.unTrash.TabIndex = 3
+        Me.unTrash.Text = "Delete Trashes folder (Kills all trashed files!)"
+        Me.unTrash.UseVisualStyleBackColor = True
+        '
+        'unFsevents
+        '
+        Me.unFsevents.AutoSize = True
+        Me.unFsevents.Checked = True
+        Me.unFsevents.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.unFsevents.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.unFsevents.Location = New System.Drawing.Point(6, 59)
+        Me.unFsevents.Name = "unFsevents"
+        Me.unFsevents.Size = New System.Drawing.Size(202, 16)
+        Me.unFsevents.TabIndex = 2
+        Me.unFsevents.Text = "Remove fsevents/Spotlight caches"
+        Me.unFsevents.UseVisualStyleBackColor = True
+        '
+        'unUnder
+        '
+        Me.unUnder.AutoSize = True
+        Me.unUnder.Checked = True
+        Me.unUnder.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.unUnder.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.unUnder.Location = New System.Drawing.Point(6, 37)
+        Me.unUnder.Name = "unUnder"
+        Me.unUnder.Size = New System.Drawing.Size(79, 16)
+        Me.unUnder.TabIndex = 1
+        Me.unUnder.Text = "Remove ._*"
+        Me.unUnder.UseVisualStyleBackColor = True
+        '
+        'unDSS
+        '
+        Me.unDSS.AutoSize = True
+        Me.unDSS.Checked = True
+        Me.unDSS.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.unDSS.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.unDSS.Location = New System.Drawing.Point(6, 15)
+        Me.unDSS.Name = "unDSS"
+        Me.unDSS.Size = New System.Drawing.Size(115, 16)
+        Me.unDSS.TabIndex = 0
+        Me.unDSS.Text = "Remove .DS_Store"
+        Me.unDSS.UseVisualStyleBackColor = True
+        '
+        'dir
+        '
+        Me.dir.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.dir.Location = New System.Drawing.Point(244, 13)
+        Me.dir.Name = "dir"
+        Me.dir.Size = New System.Drawing.Size(35, 23)
+        Me.dir.TabIndex = 2
+        Me.dir.Text = "Dir"
+        Me.dir.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(12, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(226, 23)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Select a folder"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'process
+        '
+        Me.process.Location = New System.Drawing.Point(12, 180)
+        Me.process.MarqueeAnimationSpeed = 1
+        Me.process.Name = "process"
+        Me.process.Size = New System.Drawing.Size(268, 23)
+        Me.process.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.process.TabIndex = 5
+        Me.process.Value = 50
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(0, 206)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(295, 24)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "DeMacify 1.0 by vladkorotnev, 2013"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 229)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.process)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dir)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.perform)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Did a Mac shit on your disks again?"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents perform As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents unUnder As System.Windows.Forms.CheckBox
+    Friend WithEvents unDSS As System.Windows.Forms.CheckBox
+    Friend WithEvents dir As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents folder As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents process As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents unFsevents As System.Windows.Forms.CheckBox
+    Friend WithEvents unTrash As System.Windows.Forms.CheckBox
+
+End Class
---
+++ DeMacify/Form1.vb
@@ -0,0 +1,180 @@
+﻿Public Class Form1
+    Dim allFiles As New ArrayList
+    Dim weight As Integer
+    Dim inprogress As Boolean = False
+    'Dim totality As Integer = 0
+
+    Dim sep As String = "--------------------"
+    Dim report As String = "DeMacify " + Application.ProductVersion + " report" + vbCrLf + sep + vbCrLf
+
+    Sub putReport(ByVal str As String)
+        report += str + vbCrLf
+
+    End Sub
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.MaximumSize = Me.Size
+        Me.MinimumSize = Me.Size
+
+
+    End Sub
+
+    Private Sub dir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dir.Click
+        If inprogress Then Return
+        folder.ShowNewFolderButton = False
+        If folder.ShowDialog <> Windows.Forms.DialogResult.OK Or Not My.Computer.FileSystem.DirectoryExists(folder.SelectedPath) Then perform.Enabled = False : perform.Text = "<select a folder first>" : perform.ForeColor = Color.DarkRed : Return
+        perform.Enabled = True
+        Label1.Text = folder.SelectedPath
+        perform.Text = "DeMacify!"
+        perform.ForeColor = Color.Black
+    End Sub
+    Sub recurseWeightOfDir(ByVal dir As System.IO.DirectoryInfo)
+        For Each f As IO.FileInfo In dir.GetFiles
+            weight += f.Length
+        Next
+        For Each d As IO.DirectoryInfo In dir.GetDirectories
+            recurseWeightOfDir(d)
+        Next
+    End Sub
+    Sub processFolder(ByVal dir As String)
+        Dim p As New ArrayList
+        Try
+            Dim files As System.IO.FileInfo() = FileIO.FileSystem.GetDirectoryInfo(dir).GetFiles
+            Dim dirs As System.IO.DirectoryInfo() = FileIO.FileSystem.GetDirectoryInfo(dir).GetDirectories
+
+            Application.DoEvents()
+            If dir = folder.SelectedPath Then process.Maximum = UBound(files) + UBound(dirs) + 2
+            If dir = folder.SelectedPath Then process.Value = 0
+
+            For Each file As System.IO.FileInfo In files
+                If dir = folder.SelectedPath Then
+                    Try
+                        process.Value += 1
+                    Catch ex As Exception
+
+                    End Try
+                End If
+                '  totality += 1
+                If (unDSS.Checked = True And file.Name = ".DS_Store") Then allFiles.Add("File: " + file.FullName) : weight += file.Length
+
+                If (unUnder.Checked And file.Name.StartsWith("._")) Then allFiles.Add("File: " + file.FullName) : weight += file.Length
+
+            Next
+
+           Application.DoEvents()
+ 
+            For Each d As System.IO.DirectoryInfo In dirs
+                
+
+                If (unFsevents.Checked And (d.Name = ".fseventsd" Or d.Name = ".Spotlight-V100" Or d.Name = ".TemporaryItems")) Or (unTrash.Checked And d.Name = ".Trashes") Then
+                    allFiles.Add("Dir: " + d.FullName)
+                    recurseWeightOfDir(d)
+                Else
+                    If dir = folder.SelectedPath Then
+                        Try
+                            process.Value += 1
+                        Catch ex As Exception
+
+                        End Try
+                    End If
+
+                    processFolder(d.FullName)
+                End If
+
+            Next
+
+        Catch ex As Exception
+            putReport("Error while scanning: " + ex.Message)
+        End Try
+       
+
+    End Sub
+    Sub deleteEmAll()
+        putReport("Deleting " + allFiles.Count.ToString + " files...")
+
+        For i As Integer = 0 To allFiles.Count - 1
+
+            report += allFiles.Item(i) + " : "
+            process.Value = (i / allFiles.Count) * 100
+            Try
+
+                If allFiles.Item(i).ToString.StartsWith("File: ") Then
+                    My.Computer.FileSystem.DeleteFile(allFiles.Item(i).ToString.Substring(5))
+                    putReport("Removed")
+                ElseIf allFiles.Item(i).ToString.StartsWith("Dir: ") Then
+                    My.Computer.FileSystem.DeleteDirectory(allFiles.Item(i).ToString.Substring(4), FileIO.DeleteDirectoryOption.DeleteAllContents)
+                    putReport("Removed")
+                Else
+                    putReport("Wat.")
+                End If
+
+            Catch ex As Exception
+                putReport("FAIL")
+                putReport(sep)
+                putReport(ex.Message)
+                putReport(sep)
+            End Try
+            Application.DoEvents()
+        Next
+    End Sub
+
+    Private Sub perform_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles perform.Click
+        If inprogress Then Return
+        inprogress = True
+        weight = 0
+        GroupBox1.Enabled = False
+        process.MarqueeAnimationSpeed = 100
+        putReport("Started scan of " + folder.SelectedPath + " at " + Now.ToString)
+
+        perform.ForeColor = Color.Green
+        perform.Text = "Enumerating..."
+        processFolder(folder.SelectedPath)
+        perform.Text = "Found " + allFiles.Count.ToString + " files of " + CStr(Math.Round(weight / 1024)) + "K"
+        putReport("Found  " + allFiles.Count.ToString + " files.")
+
+        process.Value = 0
+        process.Maximum = 100
+        process.Minimum = 0
+
+        If allFiles.Count = 0 Then
+            MsgBox("This folder has not been touched by a Mac yet!", MsgBoxStyle.OkOnly + MsgBoxStyle.Information, "Nothing found")
+            inprogress = False
+            GroupBox1.Enabled = True
+            Return
+        End If
+        deleteEmAll()
+
+        inprogress = False
+        putReport(sep)
+        putReport("Total freed: approx. " + CStr(Math.Round(weight / 1024)) + "K")
+
+        putReport("Total files/folders deleted: " + allFiles.Count.ToString)
+        putReport(sep)
+        putReport("-=[ DeMacify by vladkorotnev a.k.a. AkasakaRyuunosuke, 2013 ]=-")
+        perform.Text = "DeMacified with success"
+        If MsgBox("Completed. Freed about " + CStr(Math.Round(weight / 1024)) + "K. Show report?", MsgBoxStyle.YesNo + MsgBoxStyle.Information, "Done") = MsgBoxResult.Yes Then
+            '--- Begin madskillz of being too lazy to draw it in VS10
+            Dim repf As New Form
+            Dim tf As New TextBox
+            tf.Parent = repf
+            tf.Dock = DockStyle.Fill
+            tf.Multiline = True
+            tf.ScrollBars = ScrollBars.Vertical
+            tf.ReadOnly = True
+            tf.BackColor = Color.White
+            tf.Text = report
+            repf.Text = "Report"
+            repf.FormBorderStyle = Windows.Forms.FormBorderStyle.SizableToolWindow
+            repf.Show()
+
+        End If
+        report = ""
+        allFiles.Clear()
+        GroupBox1.Enabled = True
+    End Sub
+
+   
+
+    Private Sub Label2_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Label2.DoubleClick
+        Label2.Text = Label2.Text.Replace("vladkorotnev", "Akasaka Ryuunosuke")
+    End Sub
+End Class
